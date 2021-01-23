f5e70c777f7787b4ef0357910a6e3b523ad392ec
Typhoon Y <tayfunyugruk@gmail.com>
2021-01-22 22:41:42 -0500

    expo icons integrated

--- app/containers/login/login.tsx
+++ app/containers/login/login.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { View, StyleSheet, SafeAreaView, Text } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
 import C42PrimaryButton from "../../components/button/primary";
-import C42TextInput from "../../components/input/text-input";
+import C42TextInputWithIcon from "../../components/input/text-input-with-icon";
 import { changeEmail, changePassword, doLogin } from "../../redux/modules/auth/auth-reducer";
 import { RootState } from "../../redux/root-reducer";
 
@@ -37,16 +37,16 @@ export default function LoginScreen() {
             </View>
             <View style={styles.bottomContainer}>
                 <View style={{ paddingBottom: 10 }}>
-                    <C42TextInput placeholder={"Email"} value={authState.email} onChangeText={(text: string) => dispatch(changeEmail(text))} />
+                    <C42TextInputWithIcon iconName={"person"} iconSize={22} iconColor={"grey"} placeholder={"Email"} value={authState.email} onChangeText={(text: string) => dispatch(changeEmail(text))} />
                 </View>
-                <View style={{ paddingBottom: 20 }}>
-                    <C42TextInput placeholder={"Password"} secureTextEntry={true} value={authState.password} onChangeText={(text: string) => dispatch(changePassword(text))} />
+                <View style={{ paddingBottom: 15 }}>
+                    <C42TextInputWithIcon iconName={"lock-closed"} iconSize={22} iconColor={"grey"} placeholder={"Password"} secureTextEntry={true} value={authState.password} onChangeText={(text: string) => dispatch(changePassword(text))} />
                 </View>
-                <View style={{ paddingBottom: 20, marginHorizontal: 8 }}>
-                    <Text style={{ color: "black" }}>Forgot password ?</Text>
-                </View>
-                <View style={{ paddingBottom: 10 }}>
+                <View style={{ paddingBottom: 30 }}>
                     <C42PrimaryButton text={"Sign In"} onPress={() => dispatch(doLogin(authState))}></C42PrimaryButton>
+                </View>
+                <View style={{ marginHorizontal: 8 }}>
+                    <Text style={{ color: "black" }}>Forgot password ?</Text>
                 </View>
             </View>
         </View>

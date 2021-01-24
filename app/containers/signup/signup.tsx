bbd5326d20cd77fd535ea439944e63c6d62c50be
Typhoon Y <tayfunyugruk@gmail.com>
2021-01-24 18:32:03 -0500

    welcome, login, signup screen images integrated

--- app/containers/signup/signup.tsx
+++ app/containers/signup/signup.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { View, StyleSheet, SafeAreaView, Text } from "react-native";
+import { View, StyleSheet, SafeAreaView, Image } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
 import C42PrimaryButton from "../../components/button/primary";
 import C42TextInputWithIcon from "../../components/input/text-input-with-icon";
@@ -22,6 +22,7 @@ const styles = StyleSheet.create({
     },
     bottomContainer: {
         flex: 2,
+        paddingTop: 40,
         paddingHorizontal: 32
     }
 });
@@ -30,28 +31,28 @@ export default function SignupScreen() {
     const dispatch = useDispatch();
     const authState = useSelector((state: RootState) => state.auth);
 
-    return (<SafeAreaView style={styles.safeArea}>
-        <View style={styles.mainContainer}>
-            <View style={styles.topContainer}>
-                <Text style={{ color: "black" }}>WELCOME !</Text>
+    return (<View style={styles.mainContainer}>
+        <View style={styles.topContainer}>
+            <Image source={require("../../images/computer_and_coffee.jpeg")} resizeMode={"center"} style={{ flex: 1 }}>
+
+            </Image>
+        </View>
+        <View style={styles.bottomContainer}>
+            <View style={{ paddingBottom: 10 }}>
+                <C42TextInputWithIcon iconName={"person"} iconSize={22} iconColor={"grey"} placeholder={"First Name"} value={authState.email} onChangeText={(text: string) => dispatch(changeFirstName(text))} />
             </View>
-            <View style={styles.bottomContainer}>
-                <View style={{ paddingBottom: 10 }}>
-                    <C42TextInputWithIcon iconName={"person"} iconSize={22} iconColor={"grey"} placeholder={"First Name"} value={authState.email} onChangeText={(text: string) => dispatch(changeFirstName(text))} />
-                </View>
-                <View style={{ paddingBottom: 10 }}>
-                    <C42TextInputWithIcon iconName={"person"} iconSize={22} iconColor={"grey"} placeholder={"Last Name"} value={authState.email} onChangeText={(text: string) => dispatch(changeLastName(text))} />
-                </View>
-                <View style={{ paddingBottom: 10 }}>
-                    <C42TextInputWithIcon iconName={"at-circle-sharp"} iconSize={22} iconColor={"grey"} placeholder={"Email"} value={authState.email} onChangeText={(text: string) => dispatch(changeEmail(text))} />
-                </View>
-                <View style={{ paddingBottom: 30 }}>
-                    <C42TextInputWithIcon iconName={"lock-closed"} iconSize={22} iconColor={"grey"} placeholder={"Password"} secureTextEntry={true} value={authState.password} onChangeText={(text: string) => dispatch(changePassword(text))} />
-                </View>
-                <View>
-                    <C42PrimaryButton text={"Sign Up"} onPress={() => dispatch(doSignup(authState))}></C42PrimaryButton>
-                </View>
+            <View style={{ paddingBottom: 10 }}>
+                <C42TextInputWithIcon iconName={"person"} iconSize={22} iconColor={"grey"} placeholder={"Last Name"} value={authState.email} onChangeText={(text: string) => dispatch(changeLastName(text))} />
+            </View>
+            <View style={{ paddingBottom: 10 }}>
+                <C42TextInputWithIcon iconName={"at-circle-sharp"} iconSize={22} iconColor={"grey"} placeholder={"Email"} value={authState.email} onChangeText={(text: string) => dispatch(changeEmail(text))} />
+            </View>
+            <View style={{ paddingBottom: 30 }}>
+                <C42TextInputWithIcon iconName={"lock-closed"} iconSize={22} iconColor={"grey"} placeholder={"Password"} secureTextEntry={true} value={authState.password} onChangeText={(text: string) => dispatch(changePassword(text))} />
+            </View>
+            <View>
+                <C42PrimaryButton text={"Sign Up"} onPress={() => dispatch(doSignup(authState))}></C42PrimaryButton>
             </View>
         </View>
-    </SafeAreaView>);
+    </View>);
 }
